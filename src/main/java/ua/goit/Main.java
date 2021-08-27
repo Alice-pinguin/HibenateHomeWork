777060a6246d4e7b014085cbd330cdad19fdff2f
Алиса Кондратенко <78209371+Alice-pinguin@users.noreply.github.com>
2021-08-27 15:37:53 +0300

    Update Main.java

--- src/main/java/ua/goit/Main.java
+++ src/main/java/ua/goit/Main.java
@@ -19,8 +19,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hibernate tutorial");
-
         Session session = HibernateSessionFactory.getSessionFactory().openSession();
         
 
